74f76146eeca57ceac77883bd0e33aebe79da82d
Max Kuznetsov <maksjago@gmail.com>
2014-02-08 21:22:30 +0200

    refactoring

--- SMDataDiller/Code/DataProviders/SMBaseDataProvider.m
+++ SMDataDiller/Code/DataProviders/SMBaseDataProvider.m
@@ -79,39 +79,43 @@
 
 - (NSIndexPath *)indexPathOfItem:(id)item
 {
-    NSIndexPath *(^sectionBlock)(id items, NSUInteger sectionIndex) = ^(id items, NSUInteger sectionIndex) {
-        if ([items isKindOfClass:[NSArray class]]) {
-            NSArray *arrItems = (NSArray *)items;
-            for (int itemIndex = 0; itemIndex < arrItems.count; itemIndex++) {
-                id sectionItem = arrItems[itemIndex];
-                if (sectionItem == item) {
-                    return [NSIndexPath indexPathForRow:itemIndex inSection:sectionIndex];
-                }
-                
-            }
-        } else if ([self isSectionObject:items]) {
-            id<SMSectionObject> sectionObject = items;
-            NSUInteger itemIndex = [sectionObject rowForItem:item];
-            if (itemIndex != NSNotFound) {
-                return [NSIndexPath indexPathForRow:itemIndex inSection:sectionIndex];
-            }
-        }
-        return (NSIndexPath *)nil;
-    };
-    
-    NSIndexPath *itemIndexPath = sectionBlock(self.items, 0);
+    NSIndexPath *itemIndexPath = [self indexPathForItem:item fromItems:self.items withSetionIndex:0];
     if ([self hasSections]) {
         for (int sectionIndex = 0; sectionIndex < self.items.count; sectionIndex++) {
             NSArray *sectionItems = self.items[sectionIndex];
-            NSIndexPath *indexPath = sectionBlock(sectionItems, sectionIndex);
+            NSIndexPath *indexPath = [self indexPathForItem:item fromItems:sectionItems withSetionIndex:sectionIndex];
             if (indexPath) {
                 itemIndexPath = indexPath;
                 break;
             }
         }
     }
-    
     return itemIndexPath;
+}
+
+
+#pragma mark -
+#pragma mark Help Methods
+
+- (NSIndexPath *)indexPathForItem:(id)item fromItems:(id)items withSetionIndex:(NSUInteger)sectionIndex
+{
+    if ([items isKindOfClass:[NSArray class]]) {
+        NSArray *arrItems = (NSArray *)items;
+        for (int itemIndex = 0; itemIndex < arrItems.count; itemIndex++) {
+            id sectionItem = arrItems[itemIndex];
+            if (sectionItem == item) {
+                return [NSIndexPath indexPathForRow:itemIndex inSection:sectionIndex];
+            }
+            
+        }
+    } else if ([self isSectionObject:items]) {
+        id<SMSectionObject> sectionObject = items;
+        NSUInteger itemIndex = [sectionObject rowForItem:item];
+        if (itemIndex != NSNotFound) {
+            return [NSIndexPath indexPathForRow:itemIndex inSection:sectionIndex];
+        }
+    }
+    return nil;
 }
 
 - (BOOL)isSectionObject:(id)object
